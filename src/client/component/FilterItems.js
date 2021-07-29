90e441a8fedd48302fe588584fda38cc5a9b35be
Zara Ahmadi <zatokey@gmail.com>
2021-07-28 22:39:25 -0400

    create FilterItems

---
+++ src/client/component/FilterItems.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AllUsers from './AllUsers'
+
+function FilterItems() {
+    return (
+        <div>
+            <AllUsers/>
+        </div>
+    )
+}
+
+export default FilterItems
